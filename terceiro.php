388299d3e62f1a4d8184efefdcf8a0846d8bbe33
yazribeiroo <yasmim.rm05@gmail.com>
2025-02-25 22:14:36 -0300

    projeto php

---
+++ terceiro.php
@@ -0,0 +1,17 @@
+<?php 
+
+$data = "20 de agosto de 2020";
+$salario = 850.00;
+$cargo = "estágiario";
+$idade = 18;
+$resultado = true;
+
+if ($resultado) {
+    echo "verdadeiro";
+    echo "<br>";
+    echo "Seu salario é " . $salario;
+
+}
+
+
+ ?>
