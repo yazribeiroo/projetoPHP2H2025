7f780d0b2a2b84f3fd9a8b7ce80ca685467d5bf9
yazribeiroo <yasmim.rm05@gmail.com>
2025-03-24 20:21:56 -0300

    estruturas de decisão

---
+++ decimo.php
@@ -0,0 +1,36 @@
+<?php 
+
+
+    $opcao =   echo "Escolha uma opção de 1 a 5:  "
+
+    switch ($opcao) {
+
+    	case '1':
+    		echo "A opção Banana foi selecionada";
+    		break;
+
+    	case '2':
+    		echo "A opção Maçã foi selecionada";
+    		break;
+
+    	case '3':
+    		echo "A opção Melancia foi selecionada";
+    		break;
+
+    	case '4':
+    		echo "A opção Uva foi selecionada";
+    		break;
+
+    	case '5':
+    		echo "Saindo...";
+    		break;
+ 	
+    	default:
+    		echo "Opção incorreta";
+    		break;
+    }
+
+
+
+
+ ?>
---
+++ nono.php
@@ -0,0 +1,15 @@
+<?php
+
+    $nota = 8.0;
+
+    if ($nota >= 7.0) {
+    	echo "APROVADO";
+    }
+
+    else{
+    	echo "REPROVADO";
+    }
+
+
+
+  ?>
