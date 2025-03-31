fe28f14bd0cd72fc89ec545f3fd49820baa270e0
yazribeiroo <yasmim.rm05@gmail.com>
2025-03-31 20:22:35 -0300

    Atividade1PHP

---
+++ A1 quest1.php
@@ -0,0 +1,15 @@
+<?php 
+
+$num1=30;
+$num2=20;
+$soma=$num1+$num2;
+
+if ($soma >= 20) {
+	$soma = $soma + 8;
+	echo "O valor é: " . $soma;
+
+}elseif ($soma <=20) {
+	$sub = $soma - 5;
+	echo "O valor é: " . $sub;
+}
+ ?>
---
+++ A1 quest2.php
@@ -0,0 +1,37 @@
+<?php 
+
+$numero1=30;
+
+if ($numero1 % 10 == 0) {
+   echo("O numero $numero1 é divisível por 10.");
+
+}elseif ($numero1 % 5 == 0) {
+    echo("O número $numero1 é divisível por 5.");
+
+}elseif ($numero1 % 2 == 0) {
+	 echo("O número $numero1 é divisível por 2.");
+
+}else{
+	 echo("O número $numero1 não é divisivel 10, por 5 ou por 2");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ ?>
