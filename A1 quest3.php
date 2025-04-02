ff00d66a2a59d51f35ccbc7b8570a320e687c26b
yazribeiroo <yasmim.rm05@gmail.com>
2025-04-01 22:20:10 -0300

    Add files via upload

---
+++ A1 quest3.php
@@ -0,0 +1,17 @@
+<?php
+
+$nome="JULIA";
+$sexo= "F";
+$idade= 23;
+
+
+if ($sexo == "F" && $idade <= 25){
+    echo "ACEITA!";
+
+}else{
+	echo "NÃO ACEITO.";
+	
+}
+
+
+  ?>
---
+++ A1 quest4.php
@@ -0,0 +1,17 @@
+<?php  
+
+$num1= 3;
+$num2= 6;
+$num3= 1;
+$i=0;
+
+while ($i <= $num1) {
+	echo($num1);
+	$i--;
+ 	break;
+ } 
+ 
+	
+
+
+?>
